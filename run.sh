625b3734087b8b56ea39048a6b063548751cd30a
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-08-12 15:58:35 +0200

    feat: main menu

--- run.sh
+++ run.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
 
+ERR_ = ""
+
+function timedError() {
+    echo $1
+    sleep 1
+    clear
+}
+
+function mainMenu()  {
+    clear
+    echo "Main Menu :"
+    echo
+    select option in "Create new DB" "Delete DB" "Open DB" "List existing DBs" "Exit"
+    do
+        case $option in
+            "Create new DB")
+                echo started
+                timedError "Test"
+                ;;
+            "Delete DB")
+                ;;
+            "Open DB")
+                ;;
+            "List existing DBs")
+                ;;
+            "Exit")
+                exit
+                ;;
+            *)
+                echo Try Again
+        esac
+        break
+    done
+    mainMenu
+}
+
+mainMenu
